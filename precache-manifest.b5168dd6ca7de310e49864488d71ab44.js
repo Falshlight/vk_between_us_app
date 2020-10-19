8bef855af8a212efdd010266c57e3ed45f70f0b6
Falshlight <teplinskij@inbox.ru>
2020-10-19 17:04:14 +0300

    Updates

--- precache-manifest.b5168dd6ca7de310e49864488d71ab44.js
+++ precache-manifest.b5168dd6ca7de310e49864488d71ab44.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e35cca3ab45293b480d8e2ea628a26c7",
+    "revision": "1843c26f9b58b337420c03a367609580",
     "url": "/vk_between_us_app/index.html"
   },
   {
-    "revision": "09e2240e50321cabc64c",
+    "revision": "858b7783d9f61eccba9c",
     "url": "/vk_between_us_app/static/css/2.09e62520.chunk.css"
   },
   {
-    "revision": "2da1afbdc0467cf36e07",
-    "url": "/vk_between_us_app/static/css/main.a4bf8a1e.chunk.css"
+    "revision": "d7e9e97a9e7d47a00c26",
+    "url": "/vk_between_us_app/static/css/main.9e3b3c14.chunk.css"
   },
   {
-    "revision": "09e2240e50321cabc64c",
-    "url": "/vk_between_us_app/static/js/2.c715e851.chunk.js"
+    "revision": "858b7783d9f61eccba9c",
+    "url": "/vk_between_us_app/static/js/2.7925a969.chunk.js"
   },
   {
     "revision": "bf34bdb78ad4eb9503e033c32af74385",
-    "url": "/vk_between_us_app/static/js/2.c715e851.chunk.js.LICENSE.txt"
+    "url": "/vk_between_us_app/static/js/2.7925a969.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2da1afbdc0467cf36e07",
-    "url": "/vk_between_us_app/static/js/main.9a3fc555.chunk.js"
+    "revision": "d7e9e97a9e7d47a00c26",
+    "url": "/vk_between_us_app/static/js/main.6ad5c9e9.chunk.js"
   },
   {
     "revision": "9091383352f00cf1c820",
