8cebc1272627237da098241950789830a4f2c2c1
Falshlight <teplinskij@inbox.ru>
2020-10-19 16:49:11 +0300

    Updates

---
+++ precache-manifest.a835ccddf9cd5e624f25b31886320ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e92f82a6c8bf7adc3b7125d858ede3",
+    "url": "/vk_between_us_app/index.html"
+  },
+  {
+    "revision": "05cb273bcf47042a53bf",
+    "url": "/vk_between_us_app/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "05cb273bcf47042a53bf",
+    "url": "/vk_between_us_app/static/js/2.327ee8fe.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/vk_between_us_app/static/js/2.327ee8fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7b5da692fb04757356",
+    "url": "/vk_between_us_app/static/js/main.7ef22be7.chunk.js"
+  },
+  {
+    "revision": "9091383352f00cf1c820",
+    "url": "/vk_between_us_app/static/js/runtime-main.39330ae0.js"
+  }
+]);
